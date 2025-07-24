5b3b17d31b424aecf09a53318a5e1a666cc8cec0
Ando <hei.ando.14@gmail.com>
2025-07-24 16:50:35 +0300

    fix: rewritten annotation

--- src/main/java/app/bpartners/geojobs/endpoint/rest/controller/RoadContinuerController.java
+++ src/main/java/app/bpartners/geojobs/endpoint/rest/controller/RoadContinuerController.java
@@ -1,8 +1,8 @@
 package app.bpartners.geojobs.endpoint.rest.controller;
 
 import app.bpartners.geojobs.endpoint.rest.postprocessing.model.TilingConf;
-import app.bpartners.geojobs.service.RoadContinuerService;
 import app.bpartners.geojobs.service.BucketServiceRoadContinuer;
+import app.bpartners.geojobs.service.RoadContinuerService;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.SneakyThrows;
@@ -17,16 +17,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RoadContinuerController {
 
-    private RoadContinuerService roadContinuerService;
-    private BucketServiceRoadContinuer bsrc;
-    @Value("${admin.api.key}")
-    private String adminApiKey;
+  private RoadContinuerService roadContinuerService;
+  private BucketServiceRoadContinuer bsrc;
 
-    @SneakyThrows
-    @PostMapping("/roadcontinuer")
-    public String  roadContinuer(@RequestBody String geojson, @RequestParam int zoom, @RequestParam int imageSize){
-        var tilingConf = new TilingConf(zoom, imageSize);
-        var gj =  roadContinuerService.continueRoute(geojson,tilingConf);
-        return bsrc.getContinuedRoutePresignedUrl(gj, adminApiKey);
-    }
+  @Value("${admin.api.key}")
+  private String adminApiKey;
+
+  @SneakyThrows
+  @PostMapping("/roadcontinuer")
+  public String roadContinuer(
+      @RequestBody String geojson, @RequestParam int zoom, @RequestParam int imageSize) {
+    var tilingConf = new TilingConf(zoom, imageSize);
+    var gj = roadContinuerService.continueRoute(geojson, tilingConf);
+    return bsrc.getContinuedRoutePresignedUrl(gj, adminApiKey);
+  }
 }
