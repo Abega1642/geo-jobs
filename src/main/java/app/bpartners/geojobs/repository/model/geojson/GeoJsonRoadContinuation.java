e3c6f80d116417ccdfeee5613b3c2c1cf51af523
Mbomain <ma.andriamparani@gmail.com>
2025-08-12 22:00:15 +0300

    chore: rename database and refractor test integration (#17)
    * chore: rename database and refactor test integration

--- src/main/java/app/bpartners/geojobs/repository/model/geojson/GeoJsonRoadContinuation.java
+++ src/main/java/app/bpartners/geojobs/repository/model/geojson/GeoJsonRoadContinuation.java
@@ -5,6 +5,9 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.JdbcTypeCode;
+
+import static org.hibernate.type.SqlTypes.NAMED_ENUM;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -30,6 +33,10 @@ public class GeoJsonRoadContinuation {
   private Integer imageSize;
 
   @Enumerated(EnumType.STRING)
+<<<<<<< HEAD
+  @JdbcTypeCode(NAMED_ENUM)
+=======
   @Column(columnDefinition = "process_status", nullable = false)
+>>>>>>> 6b3e4f2301c126f3f4ade93214bb86d37a0a4ebd
   private RoadContinuationProcessStatus status;
 }
